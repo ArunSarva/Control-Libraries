41ee664bcb0dda6af5cdf9c8ff2b456ebd4cfdfa
Arun Kumar MR <arunkumar.mr@excelindia.com>
2023-06-22 13:00:32 +0530

    first commit with herder

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+function Footer() {
+    const [state, setState] = useState('');
+
+    useEffect(() => {
+        return () => {
+
+        }
+    }, []);
+
+    return (
+        <>
+            footer
+        </>
+    )
+}
+
+export default Footer;
---
+++ src/components/inputControl/InputControl.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+function InputControl() {
+    const [state, setState] = useState('');
+
+    useEffect(() => {
+        return () => {
+
+        }
+    }, []);
+
+    return (
+        <>
+            Welcome to Input Controler
+        </>
+    )
+}
+
+export default InputControl;
